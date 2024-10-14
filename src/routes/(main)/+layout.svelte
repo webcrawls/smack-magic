2e1b3d8a0a98d411460f78be982832ae82012137
webcrawls <github.com@kaden.sh>
2024-10-14 11:25:40 -0400

    initial commit

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,17 @@
+<div class="layout-wrapper">
+    <header>
+        <h1>MAGIC CHOP</h1>
+    </header>
+    <main>
+        <slot/>
+    </main>
+    <footer></footer>
+</div>
+
+<style>
+    header {
+        height: 10rem;
+        background-color: black;
+        color: white; 
+    }
+</style>
