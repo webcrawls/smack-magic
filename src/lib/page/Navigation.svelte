b4193947a63e6ab2cc8b45a1a2ad91a6f19e44a5
webcrawls <github.com@kaden.sh>
2024-10-16 12:37:11 -0400

    ugly basic commit

---
+++ src/lib/page/Navigation.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">HOME</a>
+    <a href="/">CUSTOMER TESTIMONIALS</a>
+    <a href="/">FREQUENTLY ASKED QUESTIONS</a>
+    <a href="/" style="font-style: italic;">"THE MAGIC BLOG"</a>
+</nav>
--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,6 +1,15 @@
+<script>
+    import Navigation from "$lib/page/Navigation.svelte";
+
+</script>
+<svelte:head>
+    <link rel="stylesheet" href="/css/theme.default.css"/>
+</svelte:head>
+
 <div class="layout-wrapper">
     <header>
         <h1>MAGIC CHOP</h1>
+        <Navigation/>
     </header>
     <main>
         <slot/>
@@ -9,9 +18,4 @@
 </div>
 
 <style>
-    header {
-        height: 10rem;
-        background-color: black;
-        color: white; 
-    }
 </style>
