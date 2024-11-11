17e2a0094d1b002a3015dc0b66f0da8ca6a42840
Kaden <69081152+webcrawls@users.noreply.github.com>
2024-11-11 21:33:48 +0000

    fix: proper og:image link

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -1,4 +1,4 @@
 <meta property="og:title" content="Smack Magic!" />
 <!-- <meta property="og:type" content="video.movie" />
 <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" /> -->
-<meta property="og:image" content="https://ia.media-imdb.com/img/logo.jpeg" />
+<meta property="og:image" content="/img/logo.jpeg" />
