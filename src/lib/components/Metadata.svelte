25659856fe2f4be40cfed74667504ada42100ae1
Kaden <69081152+webcrawls@users.noreply.github.com>
2024-11-11 21:37:18 +0000

    feat: metadata

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -1,6 +1,5 @@
 <meta property="og:title" content="Smack Magic!" />
-<!-- <meta property="og:type" content="video.movie" />
-<meta property="og:url" content="https://www.imdb.com/title/tt0117500/" /> -->
+<meta property="og:url" content="https://smack-magic.pages.dev/" />
 <meta property="og:image" content="/img/logo.jpeg" />
 <meta property="og:locale" content="en_US" />
 <meta property="og:description" content="TODO: site descrpition" />
