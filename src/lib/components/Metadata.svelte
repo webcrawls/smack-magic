9c7404f1d078d08c59477aef628035c647e89fbe
Kaden <69081152+webcrawls@users.noreply.github.com>
2024-11-11 21:36:17 +0000

    feat: opengraph tags

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -2,3 +2,5 @@
 <!-- <meta property="og:type" content="video.movie" />
 <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" /> -->
 <meta property="og:image" content="/img/logo.jpeg" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:description" content="TODO: site descrpition" />
