a648fd3929a79508ea5ab7fec5f90f33865ab444
Kaden <69081152+webcrawls@users.noreply.github.com>
2024-11-11 21:49:11 +0000

    feat: support twitter + improve og tags

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -1,5 +1,24 @@
-<meta property="og:title" content="Smack Magic!" />
-<meta property="og:url" content="https://smack-magic.pages.dev/" />
-<meta property="og:image" content="/img/logo.jpeg" />
-<meta property="og:locale" content="en_US" />
-<meta property="og:description" content="TODO: site descrpition" />
+<script>
+    let {
+        title = "Smack Magic Vegetable Chopper",
+        description = "TODO",
+        url = "https://smack-magic.pages.dev/",
+        domain = "smack-magic.pages.dev",
+        locale = "en_US",
+        logo = "https://smack-magic.pages.dev/img/logo.jpeg"
+    } = $props()
+
+</script>
+
+<meta property="og:url" content="{url}" />
+<meta property="og:title" content="{title}" />
+<meta property="og:image" content="{logo}" />
+<meta property="og:locale" content="{locale}" />
+<meta property="og:description" content="{description}" />
+
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="{domain}">
+<meta property="twitter:url" content="{url}">
+<meta name="twitter:title" content="{title}">
+<meta name="twitter:description" content="{description}">
+<meta name="twitter:image" content="{logo}">
