528900fb53c13486cfb847bd2eb862b04efc55ab
webcrawls <github.com@kaden.sh>
2024-11-22 05:58:30 -0500

    improved header legibility on desktop

--- src/lib/layouts/smack-magic-2/Header.svelte
+++ src/lib/layouts/smack-magic-2/Header.svelte
@@ -19,6 +19,7 @@
 <style>
     header {
         margin-top: 2.5rem;
+        height: 2rem;
         width: 100%;
         background: linear-gradient(to top, red 45%, rgb(255, 63, 63) 45%);
 
@@ -66,6 +67,7 @@
     }
 
     nav {
+        height: 100%;
         display: flex;
 
         & a:first-child {
@@ -73,6 +75,10 @@
         }
 
         & a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
             color: white;
             padding-inline: 1rem;
             border-right: 1px solid rgb(50, 0, 0);
@@ -81,6 +87,7 @@
         }
 
         & a:hover {
+            font-style: italic;
             background-color: rgb(255,255,255,0.25);
         }
     }
