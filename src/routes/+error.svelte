8d5eb03c8205b79ff129075f9b1393a4488aaa06
webcrawls <github.com@kaden.sh>
2024-11-22 07:26:33 -0500

    fix: error handling & build crash (closes #11)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+    import SmackMagicWrapper from "$lib/layouts/smack-magic-2/SmackMagicWrapper.svelte";
+
+</script>
+<SmackMagicWrapper>
+    <p>You shouldn't be here.</p>
+    <a href="/">Back</a>
+</SmackMagicWrapper>
