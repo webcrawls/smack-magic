8476f5607fce2e26d3f8cc20cbd8826f59b72f30
webcrawls <github.com@kaden.sh>
2024-11-22 05:42:53 -0500

    refactor truth page

--- src/lib/components/layouts/truth/TruthWrapper.svelte
+++
@@ -1,3 +0,0 @@
-<script>
-    import "$lib/"
-</script>
--- src/routes/(truth)/the-truth/+page.svelte
+++ src/routes/(truth)/the-truth/+page.svelte
@@ -1,9 +1,14 @@
 <script>
-    import "$lib/styles/truth.css"
-    import "$lib/styles/resets.css"
+    import TruthWrapper from "$lib/layouts/truth/TruthWrapper.svelte";
+    import "$lib/styles/resets.css";
 </script>
 
-<header>
-    <p>If you are reading this, be sure to copy and save it, as I've already had two websites before this taken down.</p>
-    <h1>Joe Magic is a creep.</h1>
-</header>
+<TruthWrapper>
+    <header>
+        <p>
+            If you are reading this, be sure to copy and save it, as I've
+            already had two websites before this taken down.
+        </p>
+        <h1>Joe Magic is a creep.</h1>
+    </header>
+</TruthWrapper>
