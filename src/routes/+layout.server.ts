f0f3810766929a168ea1e1fa4fa89f735832f0e0
webcrawls <github.com@kaden.sh>
2024-11-25 00:39:25 -0500

    fix: scuffed git hash fix for cf pages

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,20 @@
 import type { LayoutServerLoad } from "./$types";
-import { execSync } from 'child_process';
+import dotenv from "dotenv"
+dotenv.config()
+
+const gitHash = await (async () => {
+    if (process.env.CF_PAGES === '1') {
+        return process.env.CF_PAGES_COMMIT_SHA
+    } else 
+    {
+        return (await import("child_process"))
+        .execSync('git rev-parse HEAD')
+            .toString().trim().slice(0, 7)
+    }
+})()
 
 export const load: LayoutServerLoad = async () => {
     return {
-            build: execSync('git rev-parse HEAD')
-            .toString().trim().slice(0, 7)
+        build: gitHash
     }
 }
