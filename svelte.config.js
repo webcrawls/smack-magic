e8ad2b69c70af03c6b2382d40dca525e3ad8263a
webcrawls <github.com@kaden.sh>
2024-11-26 03:29:49 -0500

    fix adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapterAuto from "@sveltejs/adapter-auto"
+import {adapter } from "@sveltejs/adapter-auto"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapterAuto
+		adapter
 	},
 };
 
