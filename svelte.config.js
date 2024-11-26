d211feedffc917d526615379a313977763f183b3
webcrawls <github.com@kaden.sh>
2024-11-26 03:27:41 -0500

    aw

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
-// adapted from https://www.ryanfiller.com/blog/tips/conditional-sveltekit-adapters
-
-import adapterStatic from '@sveltejs/adapter-static'
-import adapterCloudflare from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapterAuto from "@sveltejs/adapter-auto"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: process.env.CF_PAGES
-			? adapterCloudflare()
-			: adapterStatic({fallback: 'index.html'}),
+		adapter: adapterAuto
 	},
 };
 
