eb0132de6da226d6950f20762d5859c440eb5412
webcrawls <github.com@kaden.sh>
2024-11-26 03:37:05 -0500

    ?? irdk what i did

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from "@sveltejs/adapter-auto"
+import adapterCloudflare from '@sveltejs/adapter-cloudflare'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapterCloudflare()
 	},
 };
 
