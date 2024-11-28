d5f51a843f542421fc404085f54798f3647ac6d0
webcrawls <github.com@kaden.sh>
2024-11-27 19:05:58 -0500

    fix: missing layout import

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import Metadata from "$lib/common/Metadata.svelte";
     import { wrapper as Wrapper, meta } from "$lib/layouts/smack-magic";
-    import Notepad from "$lib/layouts/smack-magic/Notepad.svelte";
+    import { getContext } from "svelte";
 
     let { children } = $props();
     const user = getContext("user")
