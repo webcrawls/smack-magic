bd555aec8b15fb9f342b2a37159c949df59eaaa4
webcrawls <github.com@kaden.sh>
2024-12-02 21:10:27 -0500

    🎨 improve page title design

--- src/lib/layouts/smack-magic/PageTitle.svelte
+++ src/lib/layouts/smack-magic/PageTitle.svelte
@@ -5,3 +5,11 @@
 <div class="page-title">
     {@render children?.()}
 </div>
+
+<style>
+    .page-title {
+        color: yellow;
+        text-shadow: rgba(255, 255, 255, 0.9) 0px 0px 28px;
+        text-align: center;
+    }
+    </style>
