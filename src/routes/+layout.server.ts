d8a37c0f2ec0b7ea2b851205738903b2d9502a55
webcrawls <github.com@kaden.sh>
2024-12-03 02:06:23 -0500

    🐛 git build hash not loading on dev/static/nodejs builds

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,9 +2,12 @@ import type { LayoutServerLoad } from "./$types";
 
 const hash = (async () => {
     if (process.env.VITE_MODE === "cloudflare") {
-        return process.env.CF_PAGES_COMMIT_SHA
+        return process.env.CF_PAGES_COMMIT_SHA?.substring(0, 7)
     } else {
-        
+        const { execSync } = await import("node:child_process")
+        const result = execSync("git show -s --format=%h")
+        console.log(result.toString())
+        return result.toString()
     }
 })()
 
