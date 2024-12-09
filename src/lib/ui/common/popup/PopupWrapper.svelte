96ad67df9421531efd2439b7047da58970e7f318
webcrawls <github.com@kaden.sh>
2024-12-08 21:22:21 -0500

    misc: simplify PopupWrapper

--- src/lib/ui/common/popup/PopupWrapper.svelte
+++ src/lib/ui/common/popup/PopupWrapper.svelte
@@ -1,71 +1,71 @@
 <script lang="ts">
     import { browser } from "$app/environment";
     import { popups } from "./index.svelte";
-    import { fade } from "svelte/transition"
-
-    let root: HTMLElement;
+    import { fade } from "svelte/transition";
 
     const closePopup = (popup, event) => {
         const closeIf = (popup) => {
-            event.preventDefault()
-            if (!popup.open) return
-            if (!popup.data) return;
-            if (typeof popup.data.focus === "object" && popup.data.focus.closeOnClickOut) {
-                popups.remove(popup)
+            event.preventDefault();
+            if (!popup.open || !popup.data) return;
+            if (
+                typeof popup.data.focus === "object" &&
+                popup.data.focus.closeOnClickOut
+            ) {
+                popups.remove(popup);
             }
-        }
+        };
 
-        if (popup === undefined || popup.target) { popups.popups.forEach(closeIf) } else { closeIf(popup) }
+        if (popup === undefined || popup.target) {
+            popups.popups.forEach(closeIf);
+        } else {
+            closeIf(popup);
+        }
     };
 
     $effect(() => {
         if (!browser) return;
         if (!popups.popups.length) {
-            document.body.style.overflow = "unset"
-        } else  {
-            document.body.style.overflow = "hidden"
+            document.body.style.overflow = "unset";
+        } else {
+            document.body.style.overflow = "hidden";
         }
-    })
+    });
 </script>
 
-<div class="popup-container" aria-hidden="true" bind:this={root}>
+<!-- <div class="popup-container" aria-hidden="true" bind:this={root}> -->
     {#each popups.popups as popup, index (popup.id)}
         <div
             class="popup-wrapper"
             class:darken={popup.data.focus ?? popup.data.focus?.darken ?? false}
-            in:fade={{duration: 100}}
+            in:fade={{ duration: 100 }}
             style="{popup.data.position} {popup.data.position.x
                 ? `--position-x: ${popup.data.position.x}px; --position-y: ${popup.data.position.y}px;`
                 : ``}"
             class:fit={popup.data.position.x || popup.data.position.y}
-            onclick={closePopup.bind(this, popup)}>
-            <popup.data.render id={popup.id} removeSelf={popups.remove.bind(this, popup)}/>
+            onclick={closePopup.bind(this, popup)}
+            aria-hidden="true"
+        >
+            <popup.data.render
+                id={popup.id}
+                removeSelf={popups.remove.bind(this, popup)}
+            />
         </div>
     {/each}
-</div>
+<!-- </div> -->
 
 <style>
-    .popup-container {
-        position: fixed;
-        width: 100vw;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        overflow: hidden;
-        pointer-events: none;
-        z-index: 99;
-        container-type: size;
-        overflow: auto;
-    }
-
     .popup-wrapper {
-        position: absolute;
-        top: var(--position-y, 0);
-        left: var(--position-x, 0);
+        position: fixed;
+
+        top: env(safe-area-inset-top, 0px);
+        left: env(safe-area-inset-left, 0px);
+        right: env(safe-area-inset-right, 0px);
+        bottom: env(safe-area-inset-bottom, 0px);
+        container-type: size;
+
         width: 100%;
         height: 100%;
         pointer-events: all;
-        min-height: 400px;
     }
 
     .popup-wrapper.fit {
